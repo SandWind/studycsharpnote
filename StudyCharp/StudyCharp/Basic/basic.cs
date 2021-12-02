402b8ddde8f3e73e59b366d6a2cab5466c1d9ccb
sandwind <victorp_460@163.com>
2021-12-02 17:51:32 +0800

    静态构造,索引器

---
+++ StudyCharp/StudyCharp/Basic/basic.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCharp.Basic
+{
+    //静态构造类
+    class ClassStudy
+    {
+        //静态构造函数
+        class RandomNumberClass
+        {
+            private static Random RandomKey;
+
+            static RandomNumberClass()
+            {
+                RandomKey = new Random();
+            }
+            public int getRandomNumber()
+            {
+                return RandomKey.Next();
+            }
+        }
+        //索引器应用
+        class MutilFiledIndexClass
+        {
+           public string Name;
+           private string Work;
+           public string Address;
+           public string this [int Index]
+           {
+                set
+                {
+                    switch(Index)
+                    {
+                        case 0:
+                            this.Name = value;
+                            break;
+                        case 1:
+                            this.Work = value;
+                            break;
+                        case 2:
+                            this.Address = value;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                get
+                {
+                    switch (Index)
+                    {
+                        case 0:  return  Name;
+                        case 1:  return Work;
+                        case 2:  return  Address;
+                        default:  return this.Name;
+                    }
+                }
+           }
+
+        }
+
+        private RandomNumberClass Rn;
+        private MutilFiledIndexClass Mu;
+
+        public ClassStudy()
+        {
+            Rn = new RandomNumberClass();
+            Mu = new MutilFiledIndexClass();
+        }
+        public int RandomNum()
+        {
+            return Rn.getRandomNumber();
+        }
+
+        public void getMulitFiledContent()
+        {
+            Mu[0] = "jack";
+            Mu[1] = "developer";
+            Mu[2] = "runway station";
+            Console.WriteLine("{0},{1},{2}", Mu[0], Mu[1], Mu[2]);
+        }
+    }
+
+}
---
+++ StudyCharp/StudyCharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using StudyCharp.Basic;
+namespace StudyCharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan interval = new TimeSpan(0, 0, 1);
+            Console.WriteLine("Hello World!");
+            ClassStudy stu = new ClassStudy();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine(stu.RandomNum());
+                Thread.Sleep(interval);
+            }
+
+            stu.getMulitFiledContent();
+            Console.ReadKey();
+        }
+
+        
+      
+    }   
+}
